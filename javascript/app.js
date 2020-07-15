7660e38d689dc875265f7dd02d000e2dd33e2093
Kyle <kyleclaw@gmail.com>
2020-07-14 22:02:38 -0500

    update javascript, add package.json files and smoothscroll

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+import Scrollbar from 'smooth-scrollbar';
+
+Scrollbar.init(document.querySelector('#my-scrollbar'));
+
+// const portfolio= document.getElementById("portfolio");
+// const btn = document.getElementById("view-portfolio")
+// btn.addEventListener("click",(e)=>{
+//     e.preventDefault();
+//     portfolio.classList.add("viewing");
+//     document.querySelector(".jumbo-text").style.margin = 0;
+// });
+
+// btn.addEventListener('click', () => window.scrollTo({
+//   top: 400,
+//   behavior: 'smooth',
+// }));
