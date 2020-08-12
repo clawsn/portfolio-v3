b6b15c27c10fdb3ea90b372eec939642e0d98835
Kyle Clausen <kyleclaw@gmail.com>
2020-08-12 08:16:58 -0500

    on scroll anim on navbar

--- javascript/app.js
+++ javascript/app.js
@@ -14,3 +14,15 @@ btn.addEventListener('click', () => window.scrollTo({
   top: 400,
   behavior: 'smooth',
 }));
+
+
+
+
+
+$(window).scroll(function() {
+  if($(document).scrollTop() > 50){
+    $('.nav-bar').addClass('shrink');
+  } else {
+    $('.nav-bar').removeClass('shrink');
+  }
+});
