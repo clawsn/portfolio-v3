a6393bc531842671f95b06aa9c3e5ac09566d235
Kyle Clausen <kyleclaw@gmail.com>
2020-08-18 10:24:36 -0500

    remove unused js

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,3 @@
-// import Scrollbar from 'smooth-scrollbar';
-
-// Scrollbar.init(document.querySelector('#my-scrollbar'));
-
-const portfolio= document.getElementById("portfolio");
-const btn = document.getElementById("view-portfolio")
-btn.addEventListener("click",(e)=>{
-    e.preventDefault();
-    portfolio.classList.add("viewing");
-    document.querySelector(".jumbo-text").style.margin = 0;
-});
-
-btn.addEventListener('click', () => window.scrollTo({
-  top: 400,
-  behavior: 'smooth',
-}));
-
-
-
-
-
 $(window).scroll(function() {
   if($(document).scrollTop() > 50){
     $('.nav-bar').addClass('shrink');
